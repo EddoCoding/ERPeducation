a1c45ff084168dc7a43f0a7ebc7065260e4166ab
EddoCoding <Codingeprst@gmail.com>
2023-12-24 00:16:05 +0300

    Изменил структуру класса EnrolleeViewModel: - Перестроил наследование в объект.

--- ERPeducation/ViewModels/EnrolleeViewModel.cs
+++ ERPeducation/ViewModels/EnrolleeViewModel.cs
@@ -1,24 +1,33 @@
 ﻿using ERPeducation.Common;
+using ERPeducation.Interface;
 using ERPeducation.Views.ModuleEnrolle;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ERPeducation.ViewModels
 {
-    public class EnrolleeViewModel : BaseDataForModules<TabItemEnrollee>
+    public class EnrolleeViewModel
     {
+        public BaseDataForModules<TabItemEnrollee> Data { get; set; }
+
         public EnrolleeViewModel() 
         {
-            DataForTabs = new string[] { "Личная информация", "Контактная информация", "Образование", "Направление подготовки",
-                                         "Поданные документы", "Результаты испытаний", "Заключить договор", "Печать"};
-            TabItem = new ObservableCollection<TabItemEnrollee>();
+            Data = new BaseDataForModules<TabItemEnrollee>()
+            {
+                DataForTabs = new string[] { "Личная информация", "Контактная информация", "Образование", "Направление подготовки",
+                                         "Поданные документы", "Результаты испытаний", "Заключить договор", "Печать"},
+                TabItem = new ObservableCollection<TabItemEnrollee>()
+            };
+
             UserControl[] userControls = new UserControl[] 
             { 
                 new EnrollePersonalInformationView()
             };
-            for (int i = 0; i < DataForTabs.Length; i++)
+
+            for (int i = 0; i < Data.DataForTabs.Length; i++)
             {
-                TabItem.Add(new TabItemEnrollee(DataForTabs[i], userControls[0]));
+                Data.TabItem.Add(new TabItemEnrollee(Data.DataForTabs[i], userControls[0]));
             }
         }
     }
