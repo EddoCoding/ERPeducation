77fa83bc07b1b87cc8dd4b5db48f37b1e5bc7466
EddoCoding <Codingeprst@gmail.com>
2023-12-31 02:29:06 +0300

    Доделал календарь

---
+++ ERPeducation/Common/Converters/DateTimeToStringFormatConvert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ERPeducation.Common.Converters
+{
+    public class DateTimeToStringFormatConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime && dateTime == DateTime.MinValue) return string.Empty;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue && string.IsNullOrEmpty(stringValue)) return DateTime.MinValue;
+            return value;
+        }
+    }
+}
--- ERPeducation/ViewModels/Modules/AdmissionCampaign/EnrollePersonalInformationViewModel.cs
+++ ERPeducation/ViewModels/Modules/AdmissionCampaign/EnrollePersonalInformationViewModel.cs
@@ -1,21 +1,47 @@
-﻿using System;
-using System.Windows;
+﻿using ERPeducation.Command;
+using ERPeducation.Interface;
+using System;
+using System.Windows.Input;
 
 namespace ERPeducation.ViewModels.Modules.AdmissionCampaign
 {
-    public class EnrollePersonalInformationViewModel
+    public class EnrollePersonalInformationViewModel : INPC
     {
+        #region Свойства
         public string ValueComboBox { get; set; } = string.Empty;
-        public string[] Gender { get; set; } = { "Муж", "Жен"};
-        DateTime dateOfBirth = new DateTime();
-        public DateTime DateOfBirth 
+        public string[] Gender { get; set; } = { "Муж", "Жен" };
+        DateTime dateOfBirth;
+        public DateTime DateOfBirth
         {
             get => dateOfBirth;
             set
             {
                 dateOfBirth = value;
-                MessageBox.Show($"Выбрана дата: {DateOfBirth}", "Сообщение", MessageBoxButton.OK);
+                OnPropertyChanged(nameof(DateOfBirth));
             }
+        }
+        private bool openPopup;
+        public bool OpenPopup
+        {
+            get => openPopup;
+            set
+            {
+                openPopup = value;
+                OnPropertyChanged(nameof(OpenPopup));
+            }
+        }
+        #endregion
+        #region Команды
+        public ICommand OpenPopupCommand { get; set; }
+        #endregion
+
+        public EnrollePersonalInformationViewModel()
+        {
+            OpenPopupCommand = new RelayCommand(() =>
+            {
+                if (OpenPopup == false) OpenPopup = true;
+                else OpenPopup = false;
+            });
         }
     }
 }
